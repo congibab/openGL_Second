e64cbbbce99f4aaa816433e188e8f949e7dd9d79
congibab <engljapen2@gmail.com>
2020-09-26 00:21:18 +0900

    second

--- SecondOpenGL/SecondOpenGL/Shader.cpp
+++ SecondOpenGL/SecondOpenGL/Shader.cpp
@@ -9,7 +9,6 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
 	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	
 	try
 	{
 		vShaderFile.open(vertexPath);
@@ -38,12 +37,15 @@ void Shader::use()
 
 void Shader::setBool(const std::string& name, bool value) const
 {
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
 
 void Shader::setInt(const std::string& name, int value) const
 {
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::setFloat(const std::string& name, float value) const
 {
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
