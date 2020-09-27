9a22ecac67164b5b4ca7c1ecc5b6478ae7adb9e9
congibab <engljapen2@gmail.com>
2020-09-28 03:05:52 +0900

    texture class adding

--- opengl_glad_test/opengl_glad_test/Shader/shader.fs
+++ opengl_glad_test/opengl_glad_test/Shader/shader.fs
@@ -5,9 +5,11 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 // texture sampler
-uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = texture(texture0, TexCoord) * vec4(ourColor, 1.0);
+    //FragColor = texture(texture1, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, -TexCoord), 0.2);
 }
